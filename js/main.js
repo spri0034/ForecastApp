a127baf9ff007a75ce330ec6903acddcf63e1ad7
matt1234570 <spri0034@algonquinlive.com>
2016-11-03 22:37:27 -0400

    most resent
    upload most resent changes

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
 //Query Selectors
-let ul = document.querySelector("#appHere");
+
 //Event Listeners
 //btn.addEventListener("click", getNum);
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    let url = "forcast.php"
+    let url = "js/forecast.php"
     let data = {
         method: 'post',
         mode: 'cors'
     }
     
-//    let foreCastRequest = new Request ("./forecast.php", {method: 'post', mode: 'cors'});
+//    let foreCastRequest = new Request ("js/forecast.php", {method: 'post', mode: 'cors'});
    let foreCastReq = new Request(url, data);
     fetch(foreCastReq)
         .then(function(response){
             return response.json();
+            console.log(response);
         })
         .then(function(returnedData){
-//            returnedData.hourly.data.forEach(function(item){
-//                li = document.createElement("li");
-//                li.innerHTML = item;
-//            })
+            console.log(returnedData.hourly.data[0].time);
+            for (i=0;i<returnedData.hourly.data.length -1; i++){
+                console.log(returnedData.hourly.data[i].time);
+            }
+        
+        let ul = document.getElementById("appHere");
+            returnedData.hourly.data.forEach(function(item){
+                let li = document.createElement("li");
+                li.innerHTML = moment(item.time).format('LT').local;
+                ul.appendChild(li);
+            })
         });
   });
 
